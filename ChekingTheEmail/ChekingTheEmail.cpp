5a049d1485d900e79e4c01fdc7e4c6c112b159c0
Vasily Sokolov <vasilysokolov@mail.ru>
2022-08-02 17:37:58 +0300

    make functions

--- ChekingTheEmail/ChekingTheEmail.cpp
+++ ChekingTheEmail/ChekingTheEmail.cpp
@@ -1,8 +1,23 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
+
+bool first_part_check(std::string firstPad);
+bool second_part_check(std::string secondPad);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::string email, firstPart, secondPart;
+    std::cout << "Enter you email :\n";
+    std::cin >> email;
+
+}
+
+bool first_part_check(std::string firstPad)
+{
+    return false;
+}
+
+bool second_part_check(std::string secondPad)
+{
+    return false;
 }
 
